873b7562c4b278c6ea43ca7cdfddf58d27b7248e
Konstantinos Gerodimos <konstantinos.gerodimos@nacamar.de>
2015-07-27 18:05:09 +0200

    license info

--- execute.rb
+++ execute.rb
@@ -1,3 +1,12 @@
+#
+# This file is part of the "nunguku" framework.
+#
+# (c) Konstantinos Gerodimos <kosta@gerodimos.com>
+#
+# This source file is subject to the Apache License that is bundled
+# with this source code in the file LICENSE.
+#
+
 require 'pp'
 
 # Load bootstrap
