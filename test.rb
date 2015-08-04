24ed147a70bb432a7ab57bcdb4f51ef775650775
Konstantinos Gerodimos <konstantinos.gerodimos@nacamar.de>
2015-08-04 17:45:20 +0200

    testing

--- test.rb
+++ test.rb
@@ -1,22 +1,43 @@
 require 'irb'
 
+# Syntactic.rb
+
+# The interface for syntax tools
 module Syntactic
 
 	def down
-		@text_downcase = text.downcase
+		d = Do.new
+		@text_downcase = d.do_down(text)
+	end
+
+	class Do
+
+		def initialize
+			puts "Getting initialized."
+		end
+
+		def do_down(text)
+			text.downcase
+		end
+
 	end
 
 end
 
 module Extractor
 
-	def extract
+	def extract_syntax
 		pp "Extracting.."
 	end
 
 end
 
+module BatchJobs
+end
 
+# Semantic.rb
+
+# This will be mainly the interface
 class Semantic
 
 	include Syntactic
@@ -29,6 +50,10 @@ class Semantic
 	end
 
 end
+
+# Main.rb
+
+
 
 IRB.setup nil
 IRB.conf[:MAIN_CONTEXT] = IRB::Irb.new.context
